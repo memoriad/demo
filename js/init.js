b44fcfca241930b18c6e6f4e2572903af31aa629
memoriad <mmemoriad@gmail.com>
2017-06-02 17:45:01 +0700

    sso

--- js/init.js
+++ js/init.js
@@ -4,5 +4,16 @@
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
 
+    $('select').material_select();
+
+    $('.datepicker').pickadate({
+      selectMonths: true, // Creates a dropdown to control month
+      selectYears: 15 // Creates a dropdown of 15 years to control year
+    });
+
+    $('#location').trigger('autoresize');
+
+    $('.modal').modal();
+
   }); // end of document ready
 })(jQuery); // end of jQuery name space
